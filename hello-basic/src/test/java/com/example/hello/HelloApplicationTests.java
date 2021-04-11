2627d2fdcfa9d55efbce42e3daa629d219a43b16
hongc <hongcha11@naver.com>
2021-04-11 12:25:58 +0900

    weekly2

--- hello-basic/src/test/java/com/example/hello/HelloApplicationTests.java
+++ hello-basic/src/test/java/com/example/hello/HelloApplicationTests.java
@@ -1,5 +1,8 @@
 package com.example.hello;
 
+import com.example.hello.domain.User;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -7,7 +10,24 @@ import org.springframework.boot.test.context.SpringBootTest;
 class HelloApplicationTests {
 
     @Test
-    void contextLoads() {
+    void contextLoads() throws JsonProcessingException {
+
+        System.out.println("hello");
+
+        ObjectMapper objectMapper =new ObjectMapper();
+
+        //object -> text
+        //object mapper get method를 활용한다
+        User user=new User("steve",10,"01011112222","abc");
+
+        String text=objectMapper.writeValueAsString(user);
+        System.out.println(text);
+
+        //text-> object
+        //object 디폴트 생성자가 있어야함
+
+        User objectUser=objectMapper.readValue(text,User.class);
+        System.out.println(objectUser);
     }
 
 }
